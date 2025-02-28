61e986016c7bbd3911f6c811ab09fb8f57894fdb
Jennifer Nguyen <120598876+Jennygit03@users.noreply.github.com>
2025-02-28 15:25:46 -0600

    Update tableSchema.sql

--- tableSchema.sql
+++ tableSchema.sql
@@ -1,36 +1,66 @@
--- Users Table
-CREATE TABLE Users (
+-- Users Table: stores personal information
+CREATE TABLE Users(
     user_id INT PRIMARY KEY,
-    username VARCHAR(30) NOT NULL,
-    email VARCHAR(30) NOT NULL,
-    password VARCHAR(30) NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(50) NOT NULL,
     date_joined TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
--- Categories Table
-CREATE TABLE Categories (
-    category_id INT PRIMARY KEY,
-    category_name VARCHAR(20) NOT NULL
-);
-
--- Transactions Table (for tracking expenses)
-CREATE TABLE Transactions (
+-- Income Table: track income source for financial analysis
+CREATE TABLE Income(
+    income_id INT PRIMARY KEY,
     user_id INT,
-    transaction_id INT PRIMARY KEY,
-    amount DECIMAL(10, 2) NOT NULL,
-    category_id INT,
-    date DATE NOT NULL
+    income_source VARCHAR(50) NOT NULL,
+    income_amount DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
--- Savings Goals Table
-CREATE TABLE Savings_Goals (
+-- Categories Table: defines expense and saving categories
+CREATE TABLE Categories(
+    category_id INT PRIMARY KEY,
+    category_name VARCHAR(50) NOT NULL
+);
+
+-- Savings Goals Table: tracks user's saving goals
+CREATE TABLE Savings_goals (
     goal_id INT PRIMARY KEY,
     user_id INT,
-    goal_name VARCHAR(30) NOT NULL,
+    goal_name VARCHAR(50) NOT NULL,
     target_amount DECIMAL(10, 2) NOT NULL,
-    current_amount DECIMAL(10, 2),
+    current_amount DECIMAL(10, 2) DEFAULT 0,
     category_id INT,
-    income INT,
     FOREIGN KEY (user_id) REFERENCES Users(user_id),
     FOREIGN KEY (category_id) REFERENCES Categories(category_id)
 );
+
+-- Transactions Table: tracks user's spending activity
+CREATE TABLE Transactions(
+    user_id INT,
+    transaction_id INT PRIMARY KEY,
+    transaction_amount DECIMAL(10, 2) NOT NULL,
+    category_id INT,
+    date DATE NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (category_id) REFERENCES Categories(category_id)
+);
+
+-- Bills Table: stores expenses like rent, utilities, etc
+CREATE TABLE Bills(
+    bill_id INT PRIMARY KEY,
+    user_id INT,
+    bill_name VARCHAR(30) NOT NULL,
+    bill_amount DECIMAL(10, 2) NOT NULL,
+    due_date DATE NOT NULL,
+    payment_date DATE,
+    paid_status BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+-- Bill Reminders Table: reminds user about their bill payments
+CREATE TABLE bill_reminders(
+    reminder_id INT PRIMARY KEY,
+    bill_id INT,
+    reminder_date DATE NOT NULL,
+    FOREIGN KEY (bill_id) REFERENCES Bills(bill_id)
+);
+
