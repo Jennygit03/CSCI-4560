1925945b865589199ab13f6255b972d203f3283c
Jennifer Nguyen <120598876+Jennygit03@users.noreply.github.com>
2025-02-28 17:53:55 -0600

    Update dropTables.sql

--- dropTables.sql
+++ dropTables.sql
@@ -1,6 +1,6 @@
 -- DROP TABLES in reverse order of table creation due to how contraints are figured
 
-DROP TABLE IF EXISTS bill_reminders;
+DROP TABLE IF EXISTS Bill_reminders;
 DROP TABLE IF EXISTS Bills;
 DROP TABLE IF EXISTS Transactions;
 DROP TABLE IF EXISTS Savings_goals;
