7f3dbc5b452d0bfe3b77b34f912f55f235c3cc87
Jennifer Nguyen <120598876+Jennygit03@users.noreply.github.com>
2025-04-03 16:37:28 -0500

    Update tableSchema.sql

--- tableSchema.sql
+++ tableSchema.sql
@@ -1,5 +1,5 @@
 -- Users Table: stores personal information
-CREATE TABLE Users (
+CREATE TABLE users (
     user_id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
